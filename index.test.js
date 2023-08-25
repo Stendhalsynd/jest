d1caed491afc603913cee93aeae3c732e1ea0553
Stendhalsynd <qmflf556@naver.com>
2023-08-26 01:32:29 +0900

    init

---
+++ index.test.js
@@ -0,0 +1,20 @@
+const request = require("supertest"); // For testing HTTP requests
+const app = require("./index"); // Assuming your Express app is exported from index.js
+
+// Mock the session middleware for testing
+app.use((req, res, next) => {
+  req.session = {
+    // Mock session data here
+  };
+  next();
+});
+
+describe("Express App", () => {
+  it("responds to GET /", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).toBe(200);
+    expect(response.text).toContain("Hello, world!");
+  });
+
+  // Add more test cases for different routes and functionalities
+});
